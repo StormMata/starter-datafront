413603891aa981602b5c2948ee1ad9cb2b65da8e
StormMata <storm.mata097@gmail.com>
2022-02-09 13:23:16 -0500

    commit 22-03-9

--- working_with_data/05_callbacks/02_filter.js
+++ working_with_data/05_callbacks/02_filter.js
@@ -2,6 +2,13 @@ var list = [5,8,9,2,7,6,3,1,4];
 
 function callback(item){
     // return true for included items
+    return item > 5;
 }
 
-var filtered = list.filter(callback);
+// var filtered = list.filter(callback);
+var filtered = list.filter(function callback(item){
+    // return true for included items
+    return item > 5;
+});
+
+console.log(filtered)
